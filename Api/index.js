4287019a0c18083e55ddc19e1f83f3a7a07ef4bf
etan-nestor <etannestor45@gmail.com>
2024-06-04 15:32:13 +0000

    9th

--- Api/index.js
+++ Api/index.js
@@ -11,10 +11,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin:'http://localhost:5173',
-  credentials: true,
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use('/api/records', recordRoutes);
